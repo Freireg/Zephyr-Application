50a2a2288b3b5601e4b05bc77f9397f6736a5182
Freireg <freireg1503@gmail.com>
2023-03-29 19:46:28 -0300

    serial example

--- app/src/main.c
+++ app/src/main.c
@@ -9,7 +9,7 @@
  * 
  */
 
-#include <zephyr/zephyr.h>
+#include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 // #include <zephyr/drivers/mipi_dsi.h>
 
@@ -22,12 +22,12 @@
 
 
 void firstThreadFunction(void *p, void *a, void *c);
-void secondThreadFunction(void);
+// void secondThreadFunction(void);
 
 K_THREAD_STACK_DEFINE(my_stack_area, MY_STACK_SIZE);
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+// static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 
 
 
@@ -39,13 +39,13 @@ K_THREAD_DEFINE(firstThread_id,
 								0, 
 								0);
 
-K_THREAD_DEFINE(secondThread_id, 
-								MY_STACK_SIZE, 
-								secondThreadFunction, 
-								NULL, NULL, NULL, 
-								MY_PRIORITY, 
-								0, 
-								0);
+// K_THREAD_DEFINE(secondThread_id, 
+// 								MY_STACK_SIZE, 
+// 								secondThreadFunction, 
+// 								NULL, NULL, NULL, 
+// 								MY_PRIORITY, 
+// 								0, 
+// 								0);
 
 
 void firstThreadFunction(void *p, void *a, void *c)
@@ -65,19 +65,19 @@ void firstThreadFunction(void *p, void *a, void *c)
 	}
 }
 
-void secondThreadFunction(void)
-{
-		int ret;
-	device_is_ready(led1.port);
-	ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_INACTIVE);
-	if (ret < 0) 
-	{
-		return;
-	}
+// void secondThreadFunction(void)
+// {
+// 		int ret;
+// 	device_is_ready(led1.port);
+// 	ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_INACTIVE);
+// 	if (ret < 0) 
+// 	{
+// 		return;
+// 	}
 
-	while(1)
-	{
-		gpio_pin_toggle_dt(&led1);
-		k_msleep(SLEEP_TIME_MS);
-	}
-}
+// 	while(1)
+// 	{
+// 		gpio_pin_toggle_dt(&led1);
+// 		k_msleep(SLEEP_TIME_MS);
+// 	}
+// }
